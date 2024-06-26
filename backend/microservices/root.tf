ef3775d16566b67aa3979ff361bc39b58ce3b181
Codey Suh <codeysuh2002@gmail.com>
2024-06-27 04:22:34 +1000

    removed test lambdas

--- backend/microservices/root.tf
+++ backend/microservices/root.tf
@@ -20,10 +20,6 @@ provider "aws" {
 }
 
 # Modules
-module "test" {
-    source = "./test"
-}
-
 module "auth" {
     source = "./auth"
 }
@@ -64,76 +60,6 @@ resource "aws_apigatewayv2_stage" "mewsic_stage2" {
   }
 }
 
-### Test lambda
-
-# Integration of test lambda
-resource "aws_apigatewayv2_integration" "test" {
-  api_id = aws_apigatewayv2_api.mewsic_api2.id
-
-  integration_uri    = module.test.testLambda.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "test" {
-  api_id = aws_apigatewayv2_api.mewsic_api2.id
-
-  route_key = "POST /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.test.id}"
-  authorization_type = "JWT"
-  authorizer_id = aws_apigatewayv2_authorizer.gatewayAuth.id
-}
-
-# Logging
-resource "aws_cloudwatch_log_group" "api_gw2" {
-  name = "/aws/api_gw2/${aws_apigatewayv2_api.mewsic_api2.name}"
-
-  retention_in_days = 30
-}
-
-# Permission for test lambda
-resource "aws_lambda_permission" "api_gw2" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = module.test.testLambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.mewsic_api2.execution_arn}/*/*"
-}
-
-### Auth Lambda
-
-// TODO: don't need this anymore
-resource "aws_apigatewayv2_authorizer" "auth" {
-  api_id           = aws_apigatewayv2_api.mewsic_api2.id
-
-  authorizer_type  = "JWT"
-  identity_sources = ["$request.header.Authorization"]
-  name             = "cognito-authorizer"
-
-  jwt_configuration {
-    audience = [module.auth.userPoolClient.id]
-    issuer   = "https://${module.auth.userPool.endpoint}"
-  }
-}
-
-resource "aws_apigatewayv2_integration" "auth" {
-  api_id = aws_apigatewayv2_api.mewsic_api2.id
-
-  integration_uri = module.auth.signInLambda.invoke_arn
-  integration_type = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "auth" {
-  api_id    = aws_apigatewayv2_api.mewsic_api2.id
-
-  route_key = "GET /example"
-  target = "integrations/${aws_apigatewayv2_integration.auth.id}"
-  authorization_type = "JWT"
-  authorizer_id = aws_apigatewayv2_authorizer.gatewayAuth.id
-}
-
 resource "aws_apigatewayv2_authorizer" "gatewayAuth" {
   api_id           = aws_apigatewayv2_api.mewsic_api2.id
   authorizer_type  = "JWT"
@@ -144,4 +70,11 @@ resource "aws_apigatewayv2_authorizer" "gatewayAuth" {
     audience = [module.auth.userPoolClient.id]
     issuer   = "https://${module.auth.userPool.endpoint}"
   }
+}
+
+# Logging
+resource "aws_cloudwatch_log_group" "api_gw2" {
+  name = "/aws/api_gw2/${aws_apigatewayv2_api.mewsic_api2.name}"
+
+  retention_in_days = 30
 }
