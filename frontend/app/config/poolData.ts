c61def0251f4794c5390eaef581464dc35fa07c0
Berwyn Zhu <berwynzhu@gmail.com>
2024-07-10 21:05:22 +1000

    new uesrpool test works

--- frontend/app/config/poolData.ts
+++ frontend/app/config/poolData.ts
@@ -1,6 +1,6 @@
-// import { REACT_APP_USERPOOL_ID, REACT_APP_CLIENT_ID } from '@env';
+// import { , REACT_APP_CLIENT_ID } from '@env';
 
 export const poolData = {
-    UserPoolId: process.env.REACT_APP_USERPOOL_ID,
-    ClientId: process.env.REACT_APP_CLIENT_ID,
+    UserPoolId: process.env.USERPOOL_ID,
+    ClientId: process.env.CLIENT_ID,
 };
