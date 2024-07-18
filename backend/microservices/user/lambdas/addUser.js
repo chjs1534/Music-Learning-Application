7027ecfd4163c8f3edecf1baa47616602404fe4d
Codey Suh <codeysuh2002@gmail.com>
2024-07-19 04:32:55 +1000

    fixed lambdas

--- backend/microservices/user/lambdas/addUser.js
+++ backend/microservices/user/lambdas/addUser.js
@@ -1,11 +1,109 @@
+// const aws = require('aws-sdk');
+// const dynamo = new aws.DynamoDB.DocumentClient();
+// const crypto = require("crypto");
+// const axios = require('axios');
+// const s3 = new aws.S3();
+
+// const tableName = "UserTable";
+// const bucketName = process.env.PFP_STORAGE_BUCKET;
+
+// exports.handler = async (event, context) => {
+//     let body;
+//     let statusCode = 200;
+//     const headers = {
+//         'Access-Control-Allow-Origin': '*',
+//     };
+//     let uuid = crypto.randomUUID();
+//     let item;
+
+//     try {
+//         let requestJSON = JSON.parse(event.body);
+
+//         pfp
+//         const profilePictureUrl = "https://cdn-icons-png.flaticon.com/128/847/847969.png";
+//         const response = await axios.get(profilePictureUrl, {
+//             responseType: 'arraybuffer'
+//         });
+//         const profilePictureData = Buffer.from(response.data, 'binary');
+//         const key = `${uuid}/profile-picture.jpg`;
+
+//         await s3.putObject({
+//             Bucket: bucketName,
+//             Key: key,
+//             Body: profilePictureData,
+//             ContentEncoding: 'base64',
+//             ContentType: response.headers['content-type']
+//         }).promise();
+
+//         s3Url = `https://${bucketName}.s3.amazonaws.com/${key}`;
+
+//         // add user
+//         if (requestJSON.userType == "Child") {
+//             let parentUser = await dynamo.get(
+//                 {
+//                   TableName: tableName,
+//                   Key: {
+//                     userId: requestJSON.userId
+//                   },
+//                 }
+//             ).promise();
+//             let email = parentUser.Item.email
+//             item = {
+//                 userId: uuid,
+//                 email: email,
+//                 username: requestJSON.username,
+//                 userType: requestJSON.userType,
+//                 firstName: requestJSON.firstName,
+//                 lastName: requestJSON.lastName,
+//                 aboutMe: "",
+//                 // profilePictureUrl: s3Url || ''
+//             };
+        
+//             await dynamo.put(
+//                 {
+//                     TableName: tableName,
+//                     Item: item,
+//                 }
+//             ).promise();
+//         } else {
+//             item = {
+//                 userId: uuid,
+//                 email: requestJSON.email,
+//                 username: requestJSON.username,
+//                 userType: requestJSON.userType,
+//                 firstName: requestJSON.firstName,
+//                 lastName: requestJSON.lastName,
+//                 aboutMe: "",
+//                 // profilePictureUrl: s3Url || ''
+//             };
+        
+//             await dynamo.put(
+//                 {
+//                     TableName: tableName,
+//                     Item: item,
+//                 }
+//             ).promise();
+//         }
+//     } catch (err) {
+//         statusCode = 400;
+//         body = err.message;
+//     } finally {
+//         body = {userId: item.userId, userType: item.userType, email: item.email};
+//         body = JSON.stringify(body);
+//     }
+
+//     return {
+//         statusCode,
+//         body,
+//         headers,
+//     };
+// };
+
 const aws = require('aws-sdk');
 const dynamo = new aws.DynamoDB.DocumentClient();
 const crypto = require("crypto");
-const axios = require('axios');
-const s3 = new aws.S3();
 
 const tableName = "UserTable";
-const bucketName = process.env.PFP_STORAGE_BUCKET;
 
 exports.handler = async (event, context) => {
     let body;
@@ -18,26 +116,6 @@ exports.handler = async (event, context) => {
 
     try {
         let requestJSON = JSON.parse(event.body);
-
-        // pfp
-        const profilePictureUrl = "https://cdn-icons-png.flaticon.com/128/847/847969.png";
-        const response = await axios.get(profilePictureUrl, {
-            responseType: 'arraybuffer'
-        });
-        const profilePictureData = Buffer.from(response.data, 'binary');
-        const key = `${uuid}/profile-picture.jpg`;
-
-        await s3.putObject({
-            Bucket: bucketName,
-            Key: key,
-            Body: profilePictureData,
-            ContentEncoding: 'base64',
-            ContentType: response.headers['content-type']
-        }).promise();
-
-        s3Url = `https://${bucketName}.s3.amazonaws.com/${key}`;
-
-        // add user
         if (requestJSON.userType == "Child") {
             let parentUser = await dynamo.get(
                 {
@@ -55,8 +133,7 @@ exports.handler = async (event, context) => {
                 userType: requestJSON.userType,
                 firstName: requestJSON.firstName,
                 lastName: requestJSON.lastName,
-                aboutMe: "",
-                profilePictureUrl: s3Url || ''
+                aboutMe: ""
             };
         
             await dynamo.put(
@@ -73,8 +150,7 @@ exports.handler = async (event, context) => {
                 userType: requestJSON.userType,
                 firstName: requestJSON.firstName,
                 lastName: requestJSON.lastName,
-                aboutMe: "",
-                profilePictureUrl: s3Url || ''
+                aboutMe: ""
             };
         
             await dynamo.put(
--- backend/microservices/user/lambdas/getUserId.js
+++ backend/microservices/user/lambdas/getUserId.js
@@ -1,3 +1,73 @@
+// const aws = require('aws-sdk');
+// const dynamo = new aws.DynamoDB.DocumentClient();
+
+// const tableName = "UserTable";
+
+// exports.handler = async (event, context) => {
+//     let body;
+//     let statusCode = 200;
+//     const headers = {
+//         'Access-Control-Allow-Origin': '*',
+//     };
+//     let res;
+    
+//     try {
+//         if (!event.pathParameters.username.includes("@")) body = await queryUsersByUsername(event.pathParameters.username);
+//         else body = await queryUsersByEmail(event.pathParameters.username);
+//         if (!result.Items || result.Items.length === 0) {
+//             throw new Error("No user found with the given username or email.");
+//         }
+//         const user = body.Items[0];
+//         res = {
+//             userId: user.userId,
+//             userType: user.userType
+//         };
+//     } catch (err) {
+//         statusCode = 400;
+//         body = err.message;
+//     } finally {
+//         body = JSON.stringify(res);
+//     }
+
+//     return {
+//         statusCode,
+//         body,
+//         headers,
+//     };
+// };
+
+// const queryUsersByUsername = async (username) => {
+//     // Query as username
+//     const params1 = {
+//         TableName: tableName,
+//         IndexName: "UsernameIndex",
+//         KeyConditionExpression: "#username = :username",
+//         ExpressionAttributeNames: {
+//             "#username": "username"
+//         },
+//         ExpressionAttributeValues: {
+//             ":username": username
+//         }
+//     };
+//     return await dynamo.query(params1).promise();
+// };
+
+// const queryUsersByEmail = async (email) => {
+//     // Query as email
+//     const params1 = {
+//         TableName: tableName,
+//         IndexName: "EmailIndex",
+//         KeyConditionExpression: "#email = :email",
+//         ExpressionAttributeNames: {
+//             "#email": "email"
+//         },
+//         ExpressionAttributeValues: {
+//             ":email": email
+//         }
+//     };
+//     return await dynamo.query(params1).promise();
+// };
+
 const aws = require('aws-sdk');
 const dynamo = new aws.DynamoDB.DocumentClient();
 
@@ -9,26 +79,38 @@ exports.handler = async (event, context) => {
     const headers = {
         'Access-Control-Allow-Origin': '*',
     };
-    
+    let res = {};
+
     try {
-        if (!event.pathParameters.username.includes("@")) body = await queryUsersByUsername(event.pathParameters.username);
-        else body = await queryUsersByEmail(event.pathParameters.username);
+        if (!event.pathParameters.username.includes("@")) {
+            body = await queryUsersByUsername(event.pathParameters.username);
+        } else {
+            body = await queryUsersByEmail(event.pathParameters.username);
+        }
+
+        if (!body.Items || body.Items.length === 0) {
+            throw new Error("No user found with the given username or email.");
+        }
+
+        const user = body.Items[0];
+        res = {
+            userId: user.userId,
+            userType: user.userType
+        };
     } catch (err) {
         statusCode = 400;
-        body = err.message;
-    } finally {
-        body = JSON.stringify({userId: body.Items[0].userId, userType: body.Items[0].userType});
+        res = { error: err.message };
     }
 
     return {
         statusCode,
-        body,
+        body: JSON.stringify(res),
         headers,
     };
 };
 
 const queryUsersByUsername = async (username) => {
-    // Query as username
+    // Query by username
     const params1 = {
         TableName: tableName,
         IndexName: "UsernameIndex",
@@ -44,7 +126,7 @@ const queryUsersByUsername = async (username) => {
 };
 
 const queryUsersByEmail = async (email) => {
-    // Query as email
+    // Query by email
     const params1 = {
         TableName: tableName,
         IndexName: "EmailIndex",
